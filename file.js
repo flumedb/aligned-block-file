88d3a938830ba47937714516eac27275b92b60dc
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-16 09:09:08 +1200

    let file have a write interface

--- file.js
+++ file.js
@@ -9,11 +9,30 @@ module.exports = function (file, block_size, flags) {
   else
     fd = fs.openSync(file, flags || 'r+')
 
-  return function (i, cb) {
-    var buf = new Buffer(block_size)
-    fs.read(fd, buf, 0, block_size, i*block_size, function (err, bytes_read) {
-      cb(err, buf, bytes_read)
-    })
+  return {
+    fd: fd,
+    get: function (i, cb) {
+      var buf = new Buffer(block_size)
+      fs.read(fd, buf, 0, block_size, i*block_size, function (err, bytes_read) {
+        cb(err, buf, bytes_read)
+      })
+    },
+    append: function (buf, cb) {
+      if(!offset)
+        fs.stat(file, function (err, stat) {
+          if(err) return cb(err)
+          offset = stat.size
+          write()
+        })
+      else write()
+
+      function write() {
+        fs.write(fd, buf, 0, buf.length, offset, function (err, written) {
+          console.log(err, written)
+          cb(null, offset = offset + written)
+        })
+      }
+    }
   }
 
 }
