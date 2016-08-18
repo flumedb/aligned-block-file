ea337c43ce6fe12c728d9493d8e65e4da5cce3f9
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-18 18:19:01 +1200

    handle file size

--- file.js
+++ file.js
@@ -1,39 +1,25 @@
 var fs = require('fs')
 
 module.exports = function (file, block_size, flags) {
-  var fd
-  var offset = 0
+  var self
+  var fd = fs.openSync(file, flags || 'r+')
+  var offset = fs.statSync(file).size
 
-  if(Number.isInteger(file))
-    fd = file
-  else
-    fd = fs.openSync(file, flags || 'r+')
-
-  return {
-    fd: fd,
+  return self = {
     get: function (i, cb) {
       var buf = new Buffer(block_size)
       fs.read(fd, buf, 0, block_size, i*block_size, function (err, bytes_read) {
         cb(err, buf, bytes_read)
       })
     },
+    size: function () { return offset },
     append: function (buf, cb) {
-      if(!offset)
-        fs.stat(file, function (err, stat) {
-          if(err) return cb(err)
-          offset = stat.size
-          write()
-        })
-      else write()
-
-      function write() {
-        fs.write(fd, buf, 0, buf.length, offset, function (err, written) {
-          console.log(err, written)
-          cb(null, offset = offset + written)
-        })
-      }
+      fs.write(fd, buf, 0, buf.length, offset, function (err, written) {
+        cb(null, offset = offset + written)
+      })
     }
   }
 
 }
+
 
